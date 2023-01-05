274c6196fdf32365547484205093eab877e3a54e
AnaGabiM <ana.agma98@gmail.com>
2023-01-04 21:45:13 -0300

    exercicio laços2

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,4 +4,12 @@ const array = [
     [5, 5, 12, 16, 4],
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
+
 ]
+
+
+for (i = 0; i < array.length; i++) {
+    for (n = 0; n < array[i].length; n++) {
+        console.log(`Jogador ${i + 1}: ${array[i]}`)
+    }
+}
